7c68b6853fddea6c5c9cd6fe14b547c45d3dd8c7
Daave <daave@aaathats3as.com>
2019-05-12 14:31:05 +0100

    samptcl image

---
+++ entrypoint.sh
@@ -0,0 +1,18 @@
+FROM ubuntu:18.04
+
+LABEL author="Daave" maintainer="daave@aaathats3as.com"
+
+RUN apt -y update && apt -y install curl wget software-properties-common iproute2
+
+RUN curl -s https://api.github.com/repos/Southclaws/sampctl/releases/latest | grep browser_download_url.*amd64\.deb | cut -d : -f 2,3 | tr -d \" | wget -qi - -O tmp.deb
+RUN dpkg -i tmp.deb
+RUN rm tmp.deb
+
+RUN useradd -d /home/container -m container
+USER container
+ENV HOME /home/container
+WORKDIR /home/container
+
+COPY ./entrypoint.sh /entrypoint.sh
+
+CMD ["/bin/bash", "/entrypoint.sh"]
