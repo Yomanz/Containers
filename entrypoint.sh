71abc37e7a0bb996bda318a06c1883956a6d2244
Daave <daave@aaathats3as.com>
2019-05-12 15:14:40 +0100

    server > s

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-sampctl s ensure
-sampctl s build
+sampctl server ensure
+sampctl server build
 
 ${MODIFIED_STARTUP}
